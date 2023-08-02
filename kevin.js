502d5f9345918b9c0105e74c4a76854e0bee7504
Bordinassi <123772388+jusenai2023@users.noreply.github.com>
2023-08-02 11:40:48 -0300

    Add files via upload

---
+++ kevin.js
@@ -0,0 +1,61 @@
+// Questão 01 e 02//
+const Kelvin = 293;
+/* Essa linha de codigo cria uma variavel chamada Kevin, e atribui a ela o valor 293.*/
+
+//Questão 03 e 04//
+
+let temperatura_celsius =   Kelvin - 273;
+
+console.log(Kelvin + " Kelvin é igual a " + Celsius + " graus Celsius.");
+/*Nesta linha de código, estamos declarando uma variável chamada "kelvin" 
+e atribuindo a ela o valor de 300. Isso representa uma temperatura em Kelvin que desejamos converter para Celsius. 
+O comentário acima indica que essa variável irá armazenar a temperatura em Kelvin. 
+O valor 300 é apenas um exemplo,você pode alterá-lo para a temperatura de Kelvin que deseja converter.*/
+
+//Questão 05 e 06//
+let Celsius = 27;
+let temperatura_fahrenheit = Celsius * (9 / 5) + 32;
+let resultado_fahrenheit = Math.round(temperatura_fahrenheit);
+
+/*Nesta linha de código, calculamos a temperatura em Fahrenheit a partir do valor 
+armazenado na variável "celsius". Utilizamos o tipo de variável "let" porque o 
+valor de Fahrenheit será alterado nessa etapa.*/
+
+// Questão 07 e 08//
+Celsius = 27; 
+fahrenheit = Celsius * (9 / 5) + 32;
+fahrenheit = Math.floor(fahrenheit);
+
+/* arredondando para o fahrenheit mais proximo*/
+
+// Questão 09 e 10//
+
+Celsius = 27;
+fahrenheit = Celsius * (9 / 5) + 32;
+fahrenheit = Math.floor(fahrenheit);
+
+console.log(`A temperatura é ${fahrenheit} graus Fahrenheit.`);
+
+// Questão 11 //
+Kelvin = 0;
+Celsius = Kelvin - 273;
+let fahrenheit = celsius * (9 / 5) + 32;
+fahrenheit = Math.floor(fahrenheit);
+
+console.log(`A temperatura é ${fahrenheit} graus Fahrenheit.`);
+
+// Questão 12 //
+Celsius = 27;
+fahrenheit = Celsius * (9 / 5) + 32;
+fahrenheit = Math.floor(fahrenheit);
+
+let newton = Celsius * (33 / 100);
+newton = Math.floor(newton);
+
+console.log(`A temperatura em Celsius é ${celsius} graus.`);
+console.log(`A temperatura em Fahrenheit é ${fahrenheit} graus Fahrenheit.`);
+console.log(`A temperatura em Newton é ${newton} graus Newton.`);
+
+
+
+
